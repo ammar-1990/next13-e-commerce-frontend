99b5b1a65c356379207a0967783c3b6c8d20d703
ammar <ammar.ali.haidar.1990@gmail.com>
2023-10-13 10:56:49 +0400

    work

--- app/(routes)/category/[categoryId]/loading.tsx
+++ app/(routes)/category/[categoryId]/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return(
 
-        <div className="p-6 md:p-8 lg:p-10 ">
+        <div className="p-6 md:p-8 lg:p-10 flex-1">
                
     <Skeleton className="   rounded-xl w-full aspect-square  md:aspect-[2.4/1] bg-neutral-200" />
     </div>
